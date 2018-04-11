c683ec359c596426dcac2f2bb3796e89df3300c9
chilagranate <31170204+chilagranate@users.noreply.github.com>
2018-04-10 23:02:57 -0300

    Initial commit

---
+++ app/src/main/java/com/chila/miprimerproyectoandroid/MainActivity.java
@@ -0,0 +1,34 @@
+package com.chila.miprimerproyectoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_login_btn)
+    Button button;
+    @BindView(R.id.main_title_txt)
+    TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        button.setOnClickListener(v -> showText());
+
+
+
+    }
+
+    private void showText() {
+
+    }
+}
